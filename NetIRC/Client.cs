31fcee988499c686b826cca9bb465f89dae8249d
Kevin Brown <bass5098@gmail.com>
2013-04-20 14:06:23 -0400

    Started working on connecting

--- NetIRC/Client.cs
+++ NetIRC/Client.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Net.Sockets;
+using System.Threading;
 
 namespace NetIRC
 {
     public class Client
     {
         private TcpClient Client
+        {
+            get;
+            set;
+        }
+
+        private NetworkStream Stream
+        {
+            get;
+            set;
+        }
+
+        private StreamReader Reader
+        {
+            get;
+            set;
+        }
+
+        private StreamWriter Writer
         {
             get;
             set;
@@ -29,9 +49,27 @@ namespace NetIRC
 
         }
 
-        public void Connect()
+        public async void Connect(string server, int port, bool ssl)
         {
+            this.Client = new TcpClient();
+            await this.Client.ConnectAsync(server, port);
 
+            this.Stream = this.Client.GetStream();
+
+            this.Reader = new StreamReader(this.Stream);
+            this.Writer = new StreamWriter(this.Stream);
+
+            Thread readThread = new Thread(ReadStream);
+        }
+
+        private async void ReadStream()
+        {
+            while (this.Client != null && this.Client.Connected)
+            {
+                string line = await this.Reader.ReadLineAsync();
+
+                Console.WriteLine(line);
+            }
         }
     }
 }
