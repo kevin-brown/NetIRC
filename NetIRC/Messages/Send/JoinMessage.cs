96c9831f363b72c622e951bdb282847763503317
Kevin Brown <bass5098@gmail.com>
2013-04-20 18:30:27 -0400

    Added join message

---
+++ NetIRC/Messages/Send/JoinMessage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetIRC.Messages.Send
+{
+    public class JoinMessage : SendMessage
+    {
+        private string channel;
+
+        public JoinMessage(string channel)
+        {
+            this.channel = channel;
+        }
+
+        public void Send(System.IO.StreamWriter writer)
+        {
+            writer.WriteLine("JOIN " + this.channel);
+        }
+    }
+}
