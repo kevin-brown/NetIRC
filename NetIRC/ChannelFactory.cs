78beee05985c5bdac07f1173b3f3914e7d6a5827
Kevin Brown <bass5098@gmail.com>
2013-04-21 12:29:36 -0400

    Added ChannelFactory for managing channels

---
+++ NetIRC/ChannelFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetIRC
+{
+    class ChannelFactory
+    {
+        private static Dictionary<string, Channel> Store = new Dictionary<string, Channel>();
+
+        public static Channel FromName(string name)
+        {
+            if (Store.ContainsKey(name))
+            {
+                return Store[name];
+            }
+
+            Channel channel = new Channel(name);
+            Store[name] = channel;
+
+            return channel;
+        }
+    }
+}
