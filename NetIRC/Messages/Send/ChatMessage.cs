3c46b2ca4392e16a83ff96103209caf74c0028f6
Kevin Brown <bass5098@gmail.com>
2013-04-21 17:37:38 -0400

    Added support for sending messages to a channel

---
+++ NetIRC/Messages/Send/ChatMessage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetIRC.Messages.Send
+{
+    class ChatMessage : SendMessage
+    {
+        Channel channel;
+        string message;
+
+        public ChatMessage(string channel, string message)
+        {
+            this.channel = ChannelFactory.FromName(channel);
+            this.message = message;
+        }
+
+        public ChatMessage(Channel channel, string message)
+        {
+            this.channel = channel;
+            this.message = message;
+        }
+
+        public void Send(System.IO.StreamWriter writer)
+        {
+            writer.WriteLine("PRIVMSG #{0} {1}", this.channel.Name, this.message);
+        }
+    }
+}
