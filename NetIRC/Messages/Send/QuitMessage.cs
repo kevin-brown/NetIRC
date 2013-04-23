1e5128bbfb6db9e12c3d9a8b77d8d8264dcafef6
Kevin Brown <bass5098@gmail.com>
2013-04-22 20:27:11 -0400

    Added QUIT message support

---
+++ NetIRC/Messages/Send/QuitMessage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace NetIRC.Messages.Send
+{
+    public class QuitMessage : SendMessage
+    {
+        private string message;
+
+        public QuitMessage()
+        {
+
+        }
+
+        public QuitMessage(string message)
+        {
+            this.message = message;
+        }
+
+        public void Send(System.IO.StreamWriter writer)
+        {
+            if (this.message == null)
+            {
+                writer.WriteLine("QUIT");
+            }
+            else
+            {
+                writer.WriteLine("QUIT " + this.message);
+            }
+        }
+    }
+}
