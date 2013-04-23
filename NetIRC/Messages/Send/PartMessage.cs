a052ab07a4b647d5fef42ea001d85f689071da0a
Kevin Brown <bass5098@gmail.com>
2013-04-22 20:23:02 -0400

    Add PART reason support

--- NetIRC/Messages/Send/PartMessage.cs
+++ NetIRC/Messages/Send/PartMessage.cs
@@ -9,15 +9,29 @@ namespace NetIRC.Messages.Send
     public class PartMessage : SendMessage
     {
         private string channel;
+        private string message;
 
         public PartMessage(string channel)
         {
             this.channel = channel;
         }
 
+        public PartMessage(string channel, string message)
+        {
+            this.channel = channel;
+            this.message = message;
+        }
+
         public void Send(System.IO.StreamWriter writer)
         {
-            writer.WriteLine("PART " + this.channel);
+            if (this.message == null)
+            {
+                writer.WriteLine("PART " + this.channel);
+            }
+            else
+            {
+                writer.WriteLine("PART " + this.channel + " " + this.message);
+            }
         }
     }
 }
