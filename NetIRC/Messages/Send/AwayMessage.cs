c333b517ba39ba1f5d10e07bb27ef0f3a2f5f4f4
Kevin Brown <bass5098@gmail.com>
2013-04-22 21:22:00 -0400

    Added AWAY [message] as AwayMessage and NotAwayMessage

---
+++ NetIRC/Messages/Send/AwayMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NetIRC.Messages.Send
+{
+    public class AwayMessage : SendMessage
+    {
+        public string reason;
+
+        public AwayMessage(string reason)
+        {
+            this.reason = reason;
+        }
+
+        public void Send(System.IO.StreamWriter writer)
+        {
+            writer.WriteLine("AWAY " + this.reason);
+        }
+    }
+}
