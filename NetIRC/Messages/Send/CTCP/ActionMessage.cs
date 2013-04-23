ad8e46e29363173415900b41f2f66f7155d9144e
Kevin Brown <bass5098@gmail.com>
2013-04-22 20:52:53 -0400

    Added support for CTCP ACTION messages

---
+++ NetIRC/Messages/Send/CTCP/ActionMessage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace NetIRC.Messages.Send.CTCP
+{
+    public class ActionMessage : SendMessage
+    {
+        private Channel channel;
+        private string message;
+
+        public ActionMessage(string channel, string message)
+        {
+            this.channel = ChannelFactory.FromName(channel);
+            this.message = message;
+        }
+
+        public ActionMessage(Channel channel, string message)
+        {
+            this.channel = channel;
+            this.message = message;
+        }
+
+        public void Send(System.IO.StreamWriter writer)
+        {
+            writer.WriteLine("PRIVMSG #{0} \x001" + "ACTION {1}\x001", this.channel.Name, this.message);
+        }
+    }
+}
