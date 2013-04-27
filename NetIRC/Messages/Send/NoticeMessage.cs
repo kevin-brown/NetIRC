b1bc1e482cb9fadf999ce1c6578b15e56ef19fb8
Kevin Brown <bass5098@gmail.com>
2013-04-27 15:26:48 -0400

    Added support for sending NOTICE messages

---
+++ NetIRC/Messages/Send/NoticeMessage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace NetIRC.Messages.Send
+{
+    public class NoticeMessage : SendMessage
+    {
+        private Channel channel;
+        private string message;
+
+        public NoticeMessage(string channel, string message)
+        {
+            this.channel = ChannelFactory.FromName(channel);
+            this.message = message;
+        }
+
+        public NoticeMessage(Channel channel, string message)
+        {
+            this.channel = channel;
+            this.message = message;
+        }
+
+        public void Send(System.IO.StreamWriter writer)
+        {
+            writer.WriteLine("NOTICE #{0} {1}", this.channel.Name, this.message);
+        }
+    }
+}
