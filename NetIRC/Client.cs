27a09ae9d31ff4c5cd4fc7771b1e96ee66f808be
Kevin Brown <bass5098@gmail.com>
2013-05-05 14:27:10 -0400

    Started adding documentation to Client.

--- NetIRC/Client.cs
+++ NetIRC/Client.cs
@@ -66,6 +66,13 @@ namespace NetIRC
             this.RegisterMessages();
         }
 
+        /// <summary>
+        /// Connect to a specified <paramref name="server" />.
+        /// </summary>
+        /// <param name="server">The host of the server to connect to.</param>
+        /// <param name="port">The port to connect to on the server.</param>
+        /// <param name="ssl">True for SSL, false if not.</param>
+        /// <param name="user">The NetIRC.User to use for connecting to the server.</param>
         public async void Connect(string server, int port, bool ssl, User user)
         {
             this.User = UserFactory.FromNick(user.NickName);
@@ -87,6 +94,9 @@ namespace NetIRC
             this.Send(new Messages.Send.NickMessage(this.User));
         }
 
+        /// <summary>
+        /// Disconnects the connected user forcefully.
+        /// </summary>
         public void Disconnect()
         {
             this.ReadThread.Abort();
@@ -96,22 +106,38 @@ namespace NetIRC
             this.TcpClient.Close();
         }
 
+        /// <summary>
+        /// Join a channel specified by the <paramref name="name" />.
+        /// </summary>
+        /// <param name="name">The name of the channel.</param>
         public void JoinChannel(string name)
         {
             this.JoinChannel(ChannelFactory.FromName(name));
         }
 
+        /// <summary>
+        /// Join a specified channel based on the given Channel object.
+        /// </summary>
+        /// <param name="channel">The Channel to be used for connecting.</param>
         public void JoinChannel(Channel channel)
         {
             this.Send(new Messages.Send.JoinMessage("#" + channel.Name));
             this.Send(new Messages.Send.TopicMessage("#" + channel.Name));
         }
 
+        /// <summary>
+        /// Leave a channel specified by the <paramref name="name" />.
+        /// </summary>
+        /// <param name="name">The name of the channel.</param>
         public void LeaveChannel(string name)
         {
             this.LeaveChannel(ChannelFactory.FromName(name));
         }
 
+        /// <summary>
+        /// Leave a specified channel based on the given Channel object.
+        /// </summary>
+        /// <param name="channel"></param>
         public void LeaveChannel(Channel channel)
         {
             this.Send(new Messages.Send.PartMessage("#" + channel.Name));
@@ -146,6 +172,10 @@ namespace NetIRC
             }
         }
 
+        /// <summary>
+        /// Registers a message that will be checked when new messages are received.
+        /// </summary>
+        /// <param name="type">The type of the Message object that messages will be checked against.</param>
         public void RegisterMessage(Type type)
         {
             this.RegisteredMessages.Add(type);
@@ -177,6 +207,10 @@ namespace NetIRC
             this.RegisteredMessages.Add(typeof(Messages.Receive.Numerics.NoTopic));
         }
 
+        /// <summary>
+        /// Send a message to the connected server.
+        /// </summary>
+        /// <param name="message">The NetIRC.SendMessage instance to be sent.</param>
         public void Send(Messages.SendMessage message)
         {
             MemoryStream stream = new MemoryStream();
