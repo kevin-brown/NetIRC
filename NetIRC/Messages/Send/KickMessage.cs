36d6e80f58eacbac5f8c2fc258dd7d2c58b18b45
Kevin Brown <bass5098@gmail.com>
2013-05-16 20:46:35 -0400

    Added KickMessage

---
+++ NetIRC/Messages/Send/KickMessage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NetIRC.Messages.Send
+{
+    public class KickMessage : SendMessage
+    {
+        private Channel channel;
+        private User user;
+
+        public KickMessage(Channel channel, User user)
+        {
+            this.channel = channel;
+            this.user = user;
+        }
+
+        public void Send(System.IO.StreamWriter writer)
+        {
+            writer.WriteLine("KICK #{0} {1]", channel.Name, user.NickName);
+        }
+    }
+}
