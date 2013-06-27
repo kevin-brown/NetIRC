388b77ce085ac24591d179561594d1a1998b2679
Express50 <press.ex50@gmail.com>
2013-06-27 17:18:58 -0400

    Started working on MODE messages

---
+++ NetIRC/Messages/Receive/ChannelModeMessage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetIRC.Messages.Receive
+{
+    class ChannelModeMessage : ReceiveUserMessage
+    {
+        public static bool CheckMessage(string message, Server server)
+        {
+            string[] parts = message.Split(' ');
+
+            return ReceiveUserMessage.CheckCommand(message, "MODE") &&
+                parts[2].StartsWith("#") == true; //it is a channel
+        }
+
+        public override void ProcessMessage(string message, Client client)
+        {
+            string[] parts = message.Split(' ');
+
+            User setter = ReceiveUserMessage.GetUser(message);
+            Channel channel = client.Channels[parts[2].Remove(0, 1)];
+            
+            string modes = parts[3];
+
+            //check if it's a chanusermode or just a chanmode based on the flag and # of parameters
+            channel.TriggerOnMode(modes);
+        }
+    }
+}
---
+++ NetIRC/Messages/Receive/UserModeMessage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetIRC.Messages.Receive
+{
+    class UserModeMessage : ReceiveUserMessage
+    {
+        public static bool CheckMessage(string message, Server server)
+        {
+            string[] parts = message.Split(' ');
+
+            return ReceiveUserMessage.CheckCommand(message, "MODE") &&
+                parts[2].StartsWith("#") == false; //it's not a channel
+        }
+
+        public override void ProcessMessage(string message, Client client)
+        {
+            string[] parts = message.Split(' ');
+
+            string modes = parts[3];
+
+            client.TriggerOnUserMode(modes);
+        }
+    }
+}
