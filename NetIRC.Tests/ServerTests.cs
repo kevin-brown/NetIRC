c57f2b69b67b8698873236e75d7c790d6cde5e19
Kevin Brown <bass5098@gmail.com>
2013-08-13 20:22:28 -0700

    Started working on unit tests

---
+++ NetIRC.Tests/ServerTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetIRC;
+
+namespace NetIRC.Tests
+{
+    [TestClass]
+    public class ServerTests
+    {
+        [TestMethod]
+        public void MotdTypeTest()
+        {
+            Server server = new Server("test.com", 1234);
+
+            Messages.Send.MotdMessage motd = server.Motd();
+
+            Assert.AreEqual(motd.GetType(), typeof(Messages.Send.MotdMessage));
+        }
+    }
+}
