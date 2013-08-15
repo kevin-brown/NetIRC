c7ea44b1f50d5f4d0e961d4628ead941f1b6c4b6
Kevin Brown <bass5098@gmail.com>
2013-08-14 20:10:12 -0700

    Added some more tests for Channel

--- NetIRC.Tests/ChannelTests.cs
+++ NetIRC.Tests/ChannelTests.cs
@@ -7,6 +7,15 @@ namespace NetIRC.Tests
     public class ChannelTests
     {
         [TestMethod]
+        public void InitWithTypeTest()
+        {
+            Channel channel = new Channel("test", ChannelType.Safe);
+
+            Assert.AreEqual("test", channel.Name);
+            Assert.AreEqual(ChannelType.Safe, channel.Type);
+        }
+
+        [TestMethod]
         public void InitNameNoTypeTest()
         {
             Channel channel = new Channel("test");
@@ -49,6 +58,117 @@ namespace NetIRC.Tests
 
             Assert.AreEqual("test", channel.Name);
             Assert.AreEqual(ChannelType.Unmoderated, channel.Type);
+        }
+
+        [TestMethod]
+        public void GetTopicTest()
+        {
+            Channel channel = new Channel("#channel");
+
+            Messages.Send.TopicMessage topic = channel.GetTopic();
+
+            string output = TestHelpers.GetSendMessageOutput(topic);
+
+            Assert.AreEqual("TOPIC #channel", output);
+        }
+
+        [TestMethod]
+        public void InviteTest()
+        {
+            Channel channel = new Channel("#channel");
+            User user = new User("user");
+
+            Messages.Send.InviteMessage invite = channel.Invite(user);
+
+            string output = TestHelpers.GetSendMessageOutput(invite);
+
+            Assert.AreEqual("INVITE user #channel", output);
+        }
+
+        [TestMethod]
+        public void JoinTest()
+        {
+            Channel channel = new Channel("#channel");
+
+            Messages.Send.JoinMessage join = channel.Join();
+
+            string output = TestHelpers.GetSendMessageOutput(join);
+
+            Assert.AreEqual("JOIN #channel", output);
+        }
+
+        [TestMethod]
+        public void KickTest()
+        {
+            Channel channel = new Channel("#channel");
+            User user = new User("user");
+
+            Messages.Send.KickMessage kick = channel.Kick(user);
+
+            string output = TestHelpers.GetSendMessageOutput(kick);
+
+            Assert.AreEqual("KICK #channel user", output);
+        }
+
+        [TestMethod]
+        public void KickReasonTest()
+        {
+            Channel channel = new Channel("#channel");
+            User user = new User("user");
+
+            Messages.Send.KickMessage kick = channel.Kick(user, "reason");
+
+            string output = TestHelpers.GetSendMessageOutput(kick);
+
+            Assert.AreEqual("KICK #channel user :reason", output);
+        }
+
+        [TestMethod]
+        public void PartTest()
+        {
+            Channel channel = new Channel("#channel");
+
+            Messages.Send.PartMessage part = channel.Part();
+
+            string output = TestHelpers.GetSendMessageOutput(part);
+
+            Assert.AreEqual("PART #channel", output);
+        }
+
+        [TestMethod]
+        public void PartReasonTest()
+        {
+            Channel channel = new Channel("#channel");
+
+            Messages.Send.PartMessage part = channel.Part("reason");
+
+            string output = TestHelpers.GetSendMessageOutput(part);
+
+            Assert.AreEqual("PART #channel :reason", output);
+        }
+
+        [TestMethod]
+        public void SendNoticeTest()
+        {
+            Channel channel = new Channel("#channel");
+
+            Messages.Send.NoticeMessage notice = channel.SendNotice("message");
+
+            string output = TestHelpers.GetSendMessageOutput(notice);
+
+            Assert.AreEqual("NOTICE #channel :message", output);
+        }
+
+        [TestMethod]
+        public void SendMessageTest()
+        {
+            Channel channel = new Channel("#channel");
+
+            Messages.Send.PartMessage part = channel.Part("message");
+
+            string output = TestHelpers.GetSendMessageOutput(part);
+
+            Assert.AreEqual("PRIVMSG #channel :message", output);
         }
     }
 }
