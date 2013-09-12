be9fecf889983aff9333f889e6632087e6ce401b
Yonom <sepehr1376@gmail.com>
2013-09-12 22:44:58 +0200

    Add Away support to Who, Cleanup Who code

--- NetIRC/Messages/Receive/Numerics/Who.cs
+++ NetIRC/Messages/Receive/Numerics/Who.cs
@@ -9,41 +9,36 @@ namespace NetIRC.Messages.Receive.Numerics
             return message.Command == "352";
         }
 
-        //TODO: Do some code cleanup
         public void ProcessMessage(ParsedMessage message, Client client)
         {
             Channel channel = message.GetChannel(message.Parameters[1]);
-            User oldUser = message.GetUserFromNick(message.Parameters[5]);
+            User user = message.GetUserFromNick(message.Parameters[5]);
 
-            oldUser.HostName = message.Parameters[3];
-            oldUser.UserName = message.Parameters[2];
+            user.HostName = message.Parameters[3];
+            user.UserName = message.Parameters[2];
+            user.RealName = string.Join(" ", message.Parameters.Skip(8)).Trim();
+            user.IsOperator = message.Parameters[6].Contains('*');
 
-            if (message.Parameters[6].Length > 1)
+            foreach (char c in message.Parameters[6])
             {
-                char rankChar = message.Parameters[6][1];
-
-                if (rankChar == '*')
+                switch (c)
                 {
-                    oldUser.IsOperator = true;
+                    case 'G':
+                        user.IsAway = true;
+                        break;
+                    case 'H':
+                        user.IsAway = false;
+                        break;
+                    default:
+                        if (User.RankChars.ContainsKey(c))
+                        {
+                            UserRank rank = User.RankChars[c];
 
-                    if (message.Parameters[6].Length > 2)
-                    {
-                        rankChar = message.Parameters[6][2];
-                    }
-                }
-                else
-                {
-                    oldUser.IsOperator = false;
-                }
-
-                if (rankChar != '*')
-                {
-                    UserRank rank = User.RankChars[rankChar];
-
-                    channel.SetRank(oldUser, rank);
+                            channel.SetRank(user, rank);
+                        }
+                        break;
                 }
             }
-            oldUser.RealName = string.Join(" ", message.Parameters.Skip(8)).Trim();
 
             client.TriggerOnWho(string.Join(" ", message.Parameters.Skip(3)));
             channel.TriggerOnWho(string.Join(" ", message.Parameters.Skip(3)));
