9a59c1549d097d20cba637209e206d7840606b84
Express50 <press.ex50@gmail.com>
2014-02-05 16:53:43 -0500

    Add support for sending CAP LS and CAP LIST

---
+++ NetIRC/Messages/Send/IRCv3/CapabilityListActive.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetIRC.Messages.Send.IRCv3
+{
+    public class CapabilityListActive : ISendMessage
+    {
+        public void Send(System.IO.StreamWriter writer, Client client)
+        {
+            writer.WriteLine("CAP LIST");
+        }
+    }
+}
---
+++ NetIRC/Messages/Send/IRCv3/CapabilityListSupported.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetIRC.Messages.Send.IRCv3
+{
+    public class CapabilityListSupported : ISendMessage
+    {
+        public void Send(System.IO.StreamWriter writer, Client client)
+        {
+            writer.WriteLine("CAP LS");
+        }
+    }
+}
